4ec59190dfb8d86c5837c6338093c75700b9f8de
ns8946892-a11y <ns8946892@gmail.com>
2026-04-14 08:24:13 +0530

    Array

--- arr.java
+++ arr.java
@@ -1,5 +1,30 @@
 class arr{
     public static void main(String [] args){
-       System.out.println("HELLO WORLD!");
+        // Find Max number 
+        // int arr[] = {2,12,4322,-22,32};
+        // int res = Integer.MIN_VALUE;
+        
+        // for(int i = 0;i < arr.length;i++){
+        //     if(arr[i] > res){
+        //         res = arr[i];
+        //     }
+        // }
+        // System.out.println(res);
+
+        // Print reverse a array.
+        // int arr1[] = {1,2,3,4,5,6};
+        
+        // for(int i = arr1.length - 1;i >= 0;i--){
+        //     System.out.println(arr1[i]);
+        // }
+
+
+        // Sum of array.
+        int arr[] = {1,2,3,4,5};
+        int sum = 0;
+        for(int i = 0;i < arr.length;i++){
+            sum = sum + arr[i];
+        }
+        System.out.println(sum);
     }
 }
